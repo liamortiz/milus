a22c685bfd15611ce7333c45af275d669fcf8bd5
Leamsi Escribano <leamsi.esc@gmail.com>
2020-10-03 22:35:35 -0400

    Working on the home design and components

--- src/components/Board/BoardContainer.jsx
+++ src/components/Board/BoardContainer.jsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import JobCard from './JobCard';
 
 const BoardContainer = () => {
     return (
         <div id="board-container">
-
+            <JobCard title='Applied' jobs={0}/>
+            <JobCard title='Phone' jobs={0}/>
+            <JobCard title='Interview' jobs={0}/>
+            <JobCard title='Offer' jobs={0}/>
+            <JobCard title='Rejected' jobs={0}/>
         </div>
     )
 }
