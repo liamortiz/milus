ad1367d7e485a0557c74aa44108ff4f7b62b6e0e
Leamsi Escribano <leamsi.esc@gmail.com>
2020-11-13 18:46:39 -0500

    Working sample metrics component

--- src/components/Metrics/MetricsContainer.jsx
+++ src/components/Metrics/MetricsContainer.jsx
@@ -1,39 +1,49 @@
 import React from 'react'
-import useChartConfig from 'hooks/useChartConfig'
-import Box from 'components/Box'
-import SyntaxHighlighter from 'components/SyntaxHighlighter'
-import { Chart } from 'react-charts'
-let sourceCode
-const MetricsContainer =  () => {
-  const { data, randomizeData } = useChartConfig({
-    series: 8,
-    datums: 3,
-    dataType: 'ordinal'
-  })
-  const series = React.useMemo(
-    () => ({
-      type: 'bar'
-    }),
-    []
-  )
-  const axes = React.useMemo(
-    () => [
-      { primary: true, type: 'ordinal', position: 'bottom' },
-      { position: 'left', type: 'linear', stacked: false }
-    ],
-    []
-  )
-  return (
-    <>
-      <button onClick={randomizeData}>Randomize Data</button>
-      <br />
-      <br />
-      <Box>
-        <Chart data={data} series={series} axes={axes} tooltip />
-      </Box>
-      <br />
-      <SyntaxHighlighter code={sourceCode} />
-    </>
-  )
+import { Bar } from 'react-chartjs-2';
+
+const data = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
 }
+
+const options = {
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
+  },
+}
+
+const MetricsContainer = () => (
+  <>
+    <Bar data={data} options={options} />
+  </>
+)
+
 export default MetricsContainer;
