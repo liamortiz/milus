d46c79f14ea019329ec42c5837ed61af42b92590
Leamsi Escribano <leamsi.esc@gmail.com>
2020-11-13 18:24:25 -0500

    Added react-charts

--- src/components/Metrics/MetricsContainer.jsx
+++ src/components/Metrics/MetricsContainer.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
-import DailyApplication from './DailyApplications';
-
-const MetricsContainer = () => {
-    return (
-        <div id="metrics-container">
-            <DailyApplication />
-        </div>
-    )
+import React from 'react'
+import useChartConfig from 'hooks/useChartConfig'
+import Box from 'components/Box'
+import SyntaxHighlighter from 'components/SyntaxHighlighter'
+import { Chart } from 'react-charts'
+let sourceCode
+const MetricsContainer =  () => {
+  const { data, randomizeData } = useChartConfig({
+    series: 8,
+    datums: 3,
+    dataType: 'ordinal'
+  })
+  const series = React.useMemo(
+    () => ({
+      type: 'bar'
+    }),
+    []
+  )
+  const axes = React.useMemo(
+    () => [
+      { primary: true, type: 'ordinal', position: 'bottom' },
+      { position: 'left', type: 'linear', stacked: false }
+    ],
+    []
+  )
+  return (
+    <>
+      <button onClick={randomizeData}>Randomize Data</button>
+      <br />
+      <br />
+      <Box>
+        <Chart data={data} series={series} axes={axes} tooltip />
+      </Box>
+      <br />
+      <SyntaxHighlighter code={sourceCode} />
+    </>
+  )
 }
 export default MetricsContainer;
